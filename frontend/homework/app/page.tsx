a28207529c353c6792a860d909dc70c0322b35ab
Dhavan Singhal <dhavansinghal24@gmail.com>
2024-07-21 16:30:08 +0530

    Still learning

--- frontend/homework/app/page.tsx
+++ frontend/homework/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       Checking app 
+      <Link href='/users'>Users</Link>
     </main>
   )
 }
