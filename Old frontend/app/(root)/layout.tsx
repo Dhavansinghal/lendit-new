a2c32a826bce9a84b27d2a373eeadfa19430e6b0
Dhavan Singhal <dhavansinghal24@gmail.com>
2024-08-03 15:55:50 +0530

    Starting from scratch

--- Old frontend/app/(root)/layout.tsx
+++ Old frontend/app/(root)/layout.tsx
@@ -4,9 +4,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <html>
+      <body>
     <main>
         Sidebar
         {children}
     </main>
+    </body>
+    </html>
   )
 }
